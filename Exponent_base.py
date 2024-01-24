1876dcef33e49d446e45723cb1198456744a8349
activity2 <jdegano017@gmail.com>
2024-01-24 22:45:03 +0700

    function_exponent_base

---
+++ Exponent_base.py
@@ -0,0 +1,7 @@
+def exponent(base, exp):
+    return base ** exp
+
+base_number = 3
+exponent_number = 4
+result = exponent(base_number, exponent_number)
+print(f"{base_number} raised to the power of {exponent_number} is {result}")
